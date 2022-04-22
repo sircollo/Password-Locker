22eb6c877bb5b6ed7cd6c1f2fba3e0873e61d66e
Collins Sirwani <sircollo71@gmail.com>
2022-04-22 09:36:06 +0300

    This defines instrunctions to be executed in test methods, and use TestCase method, assertEqual to check for expected results

--- user_test.py
+++ user_test.py
@@ -8,3 +8,19 @@ class TestUser(unittest.TestCase):
   Args:
   unittest.TestCase: TestCase class that helps in creating test cases.
   '''
+  def setUp(self):
+    '''
+    set up method to run before each test case
+    '''
+    self.new_user = User("admin","12345678")
+    
+  def test_init(self):
+    '''
+    test_init case to test if the object is initialized properly
+    '''
+    self.assertEqual(self.new_user.user_name,"admin")
+    self.assertEqual(self.new_user.password,"12345678")
+    
+
+if __name__=="__main__":
+  unittest.main()
