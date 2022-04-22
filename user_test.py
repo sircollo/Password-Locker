657179cf917e790412e704c08f95e5acecbe7b4b
Collins Sirwani <sircollo71@gmail.com>
2022-04-22 08:23:38 +0300

    This imports python test framework and creates TestUser subclass

---
+++ user_test.py
@@ -0,0 +1,10 @@
+import unittest
+from user import User
+
+class TestUser(unittest.TestCase):
+  '''
+  Test class that defines test cases for user class methods.
+  
+  Args:
+  unittest.TestCase: TestCase class that helps in creating test cases.
+  '''
