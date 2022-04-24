ec755c404d98e89ab4e7d8ef2e65eed66be8acb4
Collins Sirwani <sircollo71@gmail.com>
2022-04-24 13:15:10 +0300

    This will redirect a user to register if account login fails

--- main.py
+++ main.py
@@ -82,7 +82,8 @@ def main_function():
               password = generate_random_password()
               break
           else:
-              print("Invalid option")
+              print("Invalid option")                
+              
         
         
       add_user(create_new_user(user_name,password))
@@ -90,86 +91,115 @@ def main_function():
       print("="*50)
       print(f"Account Created Successfully!\nUsername:{user_name}\nPassword:{password}")
       print("="*50)
-    
+    #   return login_user(user_name,password)
+      print("Use these short codes...\nNC --- New Credentials \nFC --- Find a credential \nVC --- View Credentials \nDel--- Delete account credential \nQ  --- Quit application \n")
+      short_code = input().lower().strip()
+        
     elif short_code == "li":
         print("Login here...")
         user_name = input("Enter Username: ")
         password = input("Enter Password: ")
         login = login_user(user_name,password)
         if login_user == login:
+          print("Login Successfull")  
           print(f"{user_name}, Continue to lock your credentials")
-        while True:
-            print("Use these short codes...\nNC --- New Credentials \nFC --- Find a credential \nVC --- View Credentials \nDel--- Delete account credential \nQ  --- Quit application \n")
-            short_code = input().lower().strip()
-            if short_code == "nc":
-                print("Create New Credentials")
-                print("="*20)
-                print("Account name: ")
-                account = input().lower()
-                print("Account's username: ")
-                account_user_name = input()
-                while True:
-                    print("Choose password Type...\nCP---Enter Custom Password\nGP---Use Generated Password")
-                    password_type = input().lower().strip()
-                    if password_type == 'cp':
-                        password = input("Enter Your Own Password\n")
-                        break
-                    elif password_type == 'gp':
-                        password = generate_random_password()
-                        break
-                    else:
-                        print("Invalid Option!")
-                        
-                save_credentials(create_new_credentials(account,account_user_name,password))
-                print('\n')
-                print("*"*40)
-                print(f"Account Credential saved successfully!\nAccount: {account} \nUsername: {account_user_name}\nPassword: {password}")
-                print("*"*40)
-            elif short_code == "fc":
-                print("Enter the Account Name you want to search")
-                search_name = input().lower()
-                if find_credential(search_name):
-                    search_credential = find_credential(search_name)
-                    print(f"Account Name : {search_credential.account}")
-                    print('-' * 40)
-                    print(f"User Name: {search_credential.account_user_name} \nPassword : {search_credential.account_password}")
-                    print('-' * 40)
-                else:
-                    print("Credentials not found")
-            elif short_code == "vc":
-                if display_various_accounts():
-                    print("Your saved accounts: ")                 
-                    print('=' * 40)
-
-                    for account in display_various_accounts():
-                        print(f"Account: {account.account} \nUsername: {account_user_name}\nPassword: {account.account_password}")
-                        print('*' * 40)
-                else:
-                    print("No Credentails saved...")
-
-            elif short_code == "del":
-                print("Enter account name to delete:")
-                search_name = input().lower()
-                if find_credential(search_name):
-                    search_credential = find_credential(search_name)
-                    print("-"*40)
-                    search_credential.delete_credentials()
-                    print('\n')
-                    print(f"{search_credential.account} successfully deleted!")
-                    print('\n')
-                else:
-                    print("Account to delete not found")
-                    print("="*40)
-            elif short_code == 'q':
-                print("Are you sure you want to quit?")
-                print("Y - to quit application N- to continue")
-                short_code = input().strip().lower()
-                if short_code == "y":
+        else:
+            print("You have not registered")
+            user_name = input("Enter Username: ")
+            password = input("Enter Password: ")
+            while True:
+                print("Choose password type:\nCP---Enter Custom Pasword\nGP---Use Generated Password")
+                password_type = input().lower().strip()
+                if password_type == 'cp':
+                    password = input("Enter Password\n")
+                    break
+                elif password_type == 'gp':
+                    password = generate_random_password()
                     break
                 else:
-                    continue
+                    print("Invalid option")                
+                    
+                
+                
+            add_user(create_new_user(user_name,password))
+            # print(str(password))
+            print("="*50)
+            print(f"Account Created Successfully!\nUsername:{user_name}\nPassword:{password}")
+            print("="*50)
+            
+            
+    while True:
+        print("Use these short codes...\nNC --- New Credentials \nFC --- Find a credential \nVC --- View Credentials \nDel--- Delete account credential \nQ  --- Quit application \n")
+        short_code = input().lower().strip()
+        if short_code == "nc":
+            print("Create New Credentials")
+            print("="*20)
+            print("Account name: ")
+            account = input().lower()
+            print("Account's username: ")
+            account_user_name = input()
+            while True:
+                print("Choose password Type...\nCP---Enter Custom Password\nGP---Use Generated Password")
+                password_type = input().lower().strip()
+                if password_type == 'cp':
+                    password = input("Enter Your Own Password\n")
+                    break
+                elif password_type == 'gp':
+                    password = generate_random_password()
+                    break
+                else:
+                    print("Invalid Option!")
+                    
+            save_credentials(create_new_credentials(account,account_user_name,password))
+            print('\n')
+            print("*"*40)
+            print(f"Account Credential saved successfully!\nAccount: {account} \nUsername: {account_user_name}\nPassword: {password}")
+            print("*"*40)
+        elif short_code == "fc":
+            print("Enter the Account Name you want to search")
+            search_name = input().lower()
+            if find_credential(search_name):
+                search_credential = find_credential(search_name)
+                print(f"Account Name : {search_credential.account}")
+                print('-' * 40)
+                print(f"User Name: {search_credential.account_user_name} \nPassword : {search_credential.account_password}")
+                print('-' * 40)
             else:
-                print("Invalid option, Please choose from the options below")
+                print("Credentials not found")
+        elif short_code == "vc":
+            if display_various_accounts():
+                print("Your saved accounts: ")                 
+                print('=' * 40)
+
+                for account in display_various_accounts():
+                    print(f"Account: {account.account} \nUsername: {account_user_name}\nPassword: {account.account_password}")
+                    print('*' * 40)
+            else:
+                print("No Credentails saved...")
+
+        elif short_code == "del":
+            print("Enter account name to delete:")
+            search_name = input().lower()
+            if find_credential(search_name):
+                search_credential = find_credential(search_name)
+                print("-"*40)
+                search_credential.delete_credentials()
+                print('\n')
+                print(f"{search_credential.account} successfully deleted!")
+                print('\n')
+            else:
+                print("Account to delete not found")
+                print("="*40)
+        elif short_code == 'q':
+            print("Are you sure you want to quit?")
+            print("Y - to quit application N- to continue")
+            short_code = input().strip().lower()
+            if short_code == "y":
+                break
+            else:
+                continue
+        else:
+            print("Invalid option, Please choose from the options below")
     else:
         print("Invalid option!, Choose an item below")
         main_function()
@@ -177,7 +207,7 @@ def main_function():
                 
         
                         
-      
+        
     
     
 if __name__=="__main__":
